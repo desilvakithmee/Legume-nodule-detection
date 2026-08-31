fedb966bcd512fcedf234afd4117d4319fc8f851
Kithmee de Silva <desilvakithmee@gmail.com>
2026-08-31 15:35:15 -0500

    Update and rename predict_yolo_RGB.py to predict_RGB.py

--- yolo/predict_RGB.py
+++ yolo/predict_RGB.py
@@ -5,14 +5,14 @@ from ultralytics import YOLO
 model = YOLO("./runs/RGB_tiled_yolo11/weights/best.pt") #change model path as needed
 
 sources = [
-    "/home/labs/brooks_lab/Hyperspectral/Determinate_RGB_Full/data/images/val",
+    "data/images/val",
 ]
 
 for src in sources:
     model.predict(
         source=src,           # <-- STRING, not list
         conf=0.15,
-        iou=0.8,    # 0.7 for determinates, 0.4 for indeterminates
+        iou=0.7,    # 0.7 for determinates, 0.4 for indeterminates
         max_det=1000,
         save=True,
         save_txt=True,
